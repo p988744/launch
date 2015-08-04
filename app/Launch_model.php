4fa52811901a975bf811851690daa2cb7766e2b0
weifan <p988744@gmail.com>
2015-08-04 16:28:23 +0800

    edit view model

--- app/Launch_model.php
+++ app/Launch_model.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Launch_model extends Model
 {
-    //
+    protected $table = 'launchs';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['name', 'type', 'timming', 'is_alive', 'price', 'location'];
 }
